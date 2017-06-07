14fa50bc4d531cf95e72cdf04888010bdb4c659e
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2017-06-07 22:20:58 +0300

    Run event loop in thread so that it can be stopped
    This is kind of anti pattern. Proper way would be run the whole thing
    inside one event loop. Let's see if it can be refactored to that at some
    point

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -3,6 +3,7 @@ import unittest
 from wqueue.worker import Worker
 
 class WorkerTestCase(unittest.TestCase):
-  def test_worker_starts(self):
+  def test_worker_can_be_stopped(self):
     worker = Worker()
     worker.start()
+    worker.stop()
--- wqueue/worker.py
+++ wqueue/worker.py
@@ -1,4 +1,5 @@
 import asyncio
+import threading
 import time
 import logging
 
@@ -7,15 +8,19 @@ logger = logging.getLogger(__name__)
 class Worker(object):
   def __init__(self):
     self.event_loop = asyncio.get_event_loop()
+    self.loop_thread = threading.Thread(target=self._start_event_loop)
 
   def start(self):
-    logger.debug("Starting worker %s" % str(self))
-    self.event_loop.call_soon(self._execute)
-    self.event_loop.run_forever()
+    self.loop_thread.start()
 
   def stop(self):
     logger.debug("Stopping worker %s" % str(self))
     self.event_loop.stop()
+
+  def _start_event_loop(self):
+    logger.debug("Starting worker %s" % str(self))
+    self.event_loop.call_soon(self._execute)
+    self.event_loop.run_forever()
 
   def _execute(self):
     logger.debug("Executing something")
