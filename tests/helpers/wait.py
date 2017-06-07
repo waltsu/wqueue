6a770a120eca963f18474cff6c14ae8e59cfa6e8
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2017-06-07 23:01:32 +0300

    Non recursive version of wait_for_success

--- tests/helpers/wait.py
+++ tests/helpers/wait.py
@@ -2,14 +2,15 @@ import threading
 import time
 
 def wait_until_success(function, poll_time=0.1, poll_count=10):
-  def wait(current_count):
-    if current_count >= poll_count:
-      raise AssertionError("Waited %ss without success" % str(poll_time * poll_count))
-
+  success = False
+  for current_count in range(0, poll_count):
     try:
       function()
+      success = True
+      break
     except:
       time.sleep(poll_time)
-      wait(current_count + 1)
 
-  wait(0)
+  if not success:
+    raise AssertionError("Waited %ss without success" % str(poll_time * poll_count))
+
