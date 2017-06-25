2a42a9feefe32d32adabb9bc35086131f9da999a
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2017-06-25 18:47:28 +0300

    Test that redis adapter can listen multiple queues

--- tests/test_redis_adapter.py
+++ tests/test_redis_adapter.py
@@ -23,3 +23,22 @@ class RedisAdapterTestCase(unittest.TestCase):
       wait_until_success(lambda: self.assertEqual(mock.call_count, 2))
     finally:
       redis_adapter.stop_listening()
+
+  def test_can_listen_multiple_queues(self):
+    redis_adapter = RedisAdapter()
+
+    first_queue_mock = Mock()
+    redis_adapter.register("first_queue", first_queue_mock)
+
+    second_queue_mock = Mock()
+    redis_adapter.register("second_queue", second_queue_mock)
+
+    try:
+      redis_adapter.start_listening()
+
+      self.redis_client.rpush("first_queue", 1)
+      self.redis_client.rpush("second_queue", 1)
+      wait_until_success(lambda: self.assertTrue(first_queue_mock.called))
+      wait_until_success(lambda: self.assertTrue(second_queue_mock.called))
+    finally:
+      redis_adapter.stop_listening()
