c8af08ba9adb1529f9775519a5c1104cb82ec4f7
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2017-07-16 23:00:05 +0300

    Hook handler to WQueue

--- wqueue/__init__.py
+++ wqueue/__init__.py
@@ -3,6 +3,7 @@ import logging
 from queue import Queue
 
 from wqueue.config import set_config
+from wqueue.handlers.handler import Handler
 from wqueue.adapters.redis_adapter import RedisAdapter
 
 
@@ -16,11 +17,14 @@ class WQueue(object):
         self.message_queue = Queue()
 
         self.adapter = RedisAdapter(self.message_queue)
+        self.handler = Handler(self.message_queue)
 
     def listen_events(self, queue_name):
         def function_wrapper(function):
             self.adapter.register(queue_name, function)
+            self.handler.add_function(queue_name, function)
         return function_wrapper
 
     def start(self):
         self.adapter.start_listening()
+        self.handler.start()
