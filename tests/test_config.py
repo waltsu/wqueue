e1067256a37c8abdd498e9788a57e1e4f765230f
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2018-04-17 23:12:53 +0300

    Make sure the defaults are preserved when testing configs

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
 import unittest
 
 from wqueue import config
+from wqueue.defaults import DEFAULTS
 
 
 class ConfigTestCase(unittest.TestCase):
@@ -14,3 +15,11 @@ class ConfigTestCase(unittest.TestCase):
     def test_sets_config(self):
         config.set_config({"extra_config": "foobar"})
         self.assertEqual(config.get_config()["extra_config"], "foobar")
+
+    def test_merges_config_with_default_config(self):
+        config.set_config({"handlers": {"multi_thread": {"thread_count": 10}}})
+
+        multi_thread_config = config.get_config()["handlers"]["multi_thread"]
+        self.assertEqual(multi_thread_config["thread_count"], 10)
+        default_timeout = DEFAULTS["handlers"]["multi_thread"]["queue_listen_timeout"]
+        self.assertEqual(multi_thread_config["queue_listen_timeout"], default_timeout)
