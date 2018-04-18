1afcc8c3ae5b5a5c2c0695889dc3c18a38190707
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2018-04-18 23:32:15 +0300

    Refactor multi_thread_handler to use base_handler parent class

--- wqueue/handlers/multi_thread_handler.py
+++ wqueue/handlers/multi_thread_handler.py
@@ -1,65 +1,26 @@
 import logging
-import threading
-import queue
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 
-from wqueue.config import get_config
+from wqueue.handlers.base_handler import BaseHandler
 
 
 logger = logging.getLogger(__name__)
 
 
-class MultiThreadHandler(object):
+class MultiThreadHandler(BaseHandler):
     def __init__(self, message_queue, config=None):
-        self.functions = {}
-        self.is_running = False
-        self.message_queue = message_queue
-
-        if config is None:
-            self.config = get_config()["handlers"]["multi_thread"]
-        else:
-            self.config = config
-
-    def add_function(self, queue_name, function):
-        self.functions[queue_name] = function
-
-    def start(self):
-        logger.debug("Starting multi thread handler")
-        self.is_running = True
-
-        queue_listener_thread = self._setup_queue_listener_thread(
-            self.message_queue, self.functions
-        )
-        queue_listener_thread.start()
+        super(MultiThreadHandler, self).__init__(message_queue, config)
+        self.executor = ThreadPoolExecutor(max_workers=self.config['thread_count'])
 
     def stop(self):
-        logger.debug("Stopping multi thread handler")
-        self.is_running = False
+        super(MultiThreadHandler, self).stop()
+        self.executor.shutdown(wait=True)
 
-    def _setup_queue_listener_thread(self, message_queue, functions):
-        return threading.Thread(target=self._read_queue, args=[message_queue, functions])
-
-    def _read_queue(self, message_queue, functions):
-        thread_count = self.config["thread_count"]
-        with ThreadPoolExecutor(max_workers=thread_count) as executor:
-            while self.is_running:
-                event = self._read_event_from_queue(message_queue)
-                if event and event.queue_name in functions.keys():
-                    function = functions[event.queue_name]
-                    executor.submit(
-                        partial(self._execute_and_log_possible_exception, function, event.data)
-                    )
-                elif event is not None:
-                    logger.error("Function not found for %s" % event.queue_name)
-
-    def _read_event_from_queue(self, message_queue):
-        try:
-            event = message_queue.get(True, self.config["queue_listen_timeout"])
-        except queue.Empty:
-            event = None
-
-        return event
+    def on_event(self, function, event):
+        self.executor.submit(
+            partial(self._execute_and_log_possible_exception, function, event.data)
+        )
 
     def _execute_and_log_possible_exception(self, function, argument):
         try:
