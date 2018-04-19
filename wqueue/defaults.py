ad2f93637136efe33bfe48f778d61a8debf7385d
Valtteri Virtanen <valtteri.virtanen@gmail.com>
2018-04-19 07:30:03 +0300

    Add default configs to multi_process_handler

--- wqueue/defaults.py
+++ wqueue/defaults.py
@@ -9,6 +9,10 @@ DEFAULTS = {
         "multi_thread": {
             "thread_count": 5,
             "queue_listen_timeout": 1
+        },
+        "multi_process": {
+            "process_count": 2,
+            "queue_listen_timeout": 1
         }
     }
 }
